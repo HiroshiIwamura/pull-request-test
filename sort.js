e302a72eafabf0a7330acd07297eaaba648c8158
HiroshiIwamura <70414385+HiroshiIwamura@users.noreply.github.com>
2021-01-14 06:17:09 +0900

    Create New file
    Create New file

---
+++ sort.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>sort.js</title>
+</head>
+<body>
+<!-- ここにHTMLを書くする -->
+<script>
+  var number = [19, 3, 81, 1, 24, 21];
+  console.log(number);
+</script>
+</body>
+</html>
